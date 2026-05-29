cc88c5d6a2e849120d028f5055d4bc5f905d3279
ATRedshaw <redshawalex02@outlook.com>
2026-05-29 14:28:26 +0100

    Add GuestRoute and ProtectedRoute components for route protection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import Layout from './components/Layout.jsx'
+import Layout         from './components/Layout.jsx'
+import GuestRoute     from './components/GuestRoute.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
 import App    from './App.jsx'
 import Login  from './pages/Login.jsx'
 import SignUp from './pages/SignUp.jsx'
@@ -13,10 +15,17 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/"       element={<App />}    />
-          <Route path="/login"  element={<Login />}  />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home"   element={<Home />}   />
+          {/* Unauthenticated only */}
+          <Route element={<GuestRoute />}>
+            <Route path="/"       element={<App />}    />
+            <Route path="/login"  element={<Login />}  />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+
+          {/* Authenticated only */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
